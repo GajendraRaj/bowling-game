2660cae1180c2b40d142b55c372508371a450754
Gajendra Rajput <Gajendra.rajput@tcs.com>
2020-05-29 16:01:46 +0530

    feat: Display 2 columns in each frame

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -15,4 +15,13 @@ describe("App component", () => {
   it("should display 10 frames in a table structure", () => {
     expect(wrapper.find(".scoreBoard .frame").length).toBe(10);
   });
+
+  it("should display 2 columns in each frame", () => {
+    const columns = wrapper
+      .find(".scoreBoard .frame")
+      .at(0)
+      .childAt(1)
+      .find("span").length;
+    expect(columns).toBe(2);
+  });
 });
