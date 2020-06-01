d9881996a7ca9d04cc612ff25dcb86b17ea7cc8f
Gajendra Rajput <Gajendra.rajput@tcs.com>
2020-06-01 18:31:09 +0530

    refactor: Simplify the logic to get roll2 value

--- src/components/frame/index.js
+++ src/components/frame/index.js
@@ -8,18 +8,29 @@ const Frame = (props) => {
     return roll === 10;
   };
 
+  const isSpare = (frame) => {
+    return frame.roll1 + frame.roll2 === 10;
+  };
+
+  const getRoll2Value = (frame) => {
+    let roll2Value;
+    if (isStrike(frame.roll1)) {
+      roll2Value = "";
+    } else if (isSpare(frame)) {
+      roll2Value = "/";
+    } else {
+      roll2Value = frame.roll2;
+    }
+
+    return roll2Value;
+  };
+
   return (
     <div className="frame">
       <p>{`frame ${props.index}`}</p>
       <div className="pins">
         <span>{isStrike(props.roll1) ? Constants.STRIKE : props.roll1}</span>
-        <span>
-          {isStrike(props.roll1)
-            ? ""
-            : props.roll1 + props.roll2 === 10
-            ? "/"
-            : props.roll2}
-        </span>
+        <span>{getRoll2Value(props)}</span>
       </div>
     </div>
   );
