72e3ab63c6b8228ef25d6c28f3523e5ea82435e3
Gajendra Rajput <Gajendra.rajput@tcs.com>
2020-06-01 17:55:11 +0530

    refactor: Extract common logic in seprate method

--- src/components/frame/index.js
+++ src/components/frame/index.js
@@ -4,12 +4,16 @@ import "../App.css";
 import Constants from "../../constants";
 
 const Frame = (props) => {
+  const isStrike = (roll) => {
+    return roll === 10;
+  };
+
   return (
     <div className="frame">
       <p>{`frame ${props.index}`}</p>
       <div className="pins">
-        <span>{props.roll1 === 10 ? Constants.STRIKE : props.roll1}</span>
-        <span>{props.roll1 === 10 ? "" : props.roll2}</span>
+        <span>{isStrike(props.roll1) ? Constants.STRIKE : props.roll1}</span>
+        <span>{isStrike(props.roll1) ? "" : props.roll2}</span>
       </div>
     </div>
   );
